fc2f4b5c2481300a714b6590b132b01c901d2b8e
Drielly <drielly.farias@sou.inteli.edu.br>
2023-03-16 15:16:32 -0300

    html

--- SEMANA_07/02_ATIVIDADES/Entrega - semana 7/Player2.gd
+++ SEMANA_07/02_ATIVIDADES/Entrega - semana 7/Player2.gd
@@ -4,3 +4,8 @@ func _physics_process(delta):
 	var y = position.y
 	var mouse_x = get_global_mouse_position().x
 	set_position(Vector2(mouse_x,y))
+func _input(event):
+	if event is InputEventMouseButton:
+		print(event.position)
+	elif event is InputEventMouseMotion:
+		print(event.position)
