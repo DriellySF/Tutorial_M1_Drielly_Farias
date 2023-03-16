d1b241e7547353ac9cf0516388edae162aa95928
Drielly <drielly.farias@sou.inteli.edu.br>
2023-03-16 10:44:05 -0300

    html e comments

---
+++ SEMANA_07/02_ATIVIDADES/Entrega - semana 7/telaInicial.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Main.tscn")#muda a cena
+#AO ENTRAR NO TREM, VOCÊ PRECISARÁ LIDAR COM O TÉDIO. A ÚNICA FORMA DE ALIVIAR ESSA DECEPÇÃO DURANTE A VIAGEM É POR MEIO DE UM JOGO. OU SEJA, O TEMA DESSE JOGO É DECEPÇÃO E COMO ATENUA-LÁ POR MEIO DA DIVERSÃO. USE O MOUSE
+ #eu fiz com base nesse tutorial: https://youtu.be/RS0y2C2C0mc
